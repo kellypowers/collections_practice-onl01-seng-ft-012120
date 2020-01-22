213d2cb7d65f18847df3f238bf59a7b0f4c1fc8b
Kelly Powers <kpowers.nj@gmail.com>
2020-01-22 23:32:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -51,9 +51,8 @@ def find_a(array)
   puts "original array #{array}"
   array.map do |i| 
     puts " i is #{i} i0 is #{i[0]}"
-    if i[0] == 'a'
-      i
-      #array.delete(i)
+    if i[0] != 'a'
+      array[i].delete
     end
   end
   puts array
